f749451b2dba95e89e3b82e4ba5bca2c796e51ea
nicco <mannythemanmoth@gmail.com>
2023-07-13 21:30:06 -0400

    added additional boiler plate code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+const cal = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');

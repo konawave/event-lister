8fa0236dac3ffa15041f4939c2cdfddeba26d30a
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-17 18:56:42 -0500

    tests

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.post('/login', async (req, res) => {
         if (!calUserData) {
             res
                 .status(400)
-                .json({ message: 'Incorrect email or password. Please try again!' });
+                .json({ message: 'Incorrect username or password. Please try again!' });
             return;
         }
 

18917197d4057e200d66a6cfa582da8d732432c7
nicco <mannythemanmoth@gmail.com>
2023-07-17 19:17:36 -0400

    server js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,7 @@
 const router = require('express').Router();
 
-<<<<<<< HEAD
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
-=======
 const userRoutes = require('./user-routes');
 
 router.use('/users', userRoutes);
 
 module.exports = router;
->>>>>>> 2b863af523f547984b272d4659efca846200b821

664a124f78a65d5f47cb1baefdd1f33ea5a2f663
nicco <mannythemanmoth@gmail.com>
2023-07-17 20:00:51 -0400

    resolved email to username issues

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,6 @@ router.post('/', async (req, res) => {
     try {
         const calUserData = await User.create({
             username: req.body.username,
-            email: req.body.email,
             password: req.body.password,
         });
 
@@ -27,14 +26,14 @@ router.post('/login', async (req, res) => {
     try {
         const calUserData = await User.findOne({
             where: {
-                email: req.body.email,
+                username: req.body.username,
             },
         });
 
         if (!calUserData) {
             res
                 .status(400)
-                .json({ message: 'Incorrect email or password. Please try again!' });
+                .json({ message: 'Incorrect Username or password. Please try again!' });
             return;
         }
 

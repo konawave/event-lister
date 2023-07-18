524e055eeb1862504e8db4c73725d38b190080d6
CrisesAverting <128323818+CrisesAverting@users.noreply.github.com>
2023-07-18 19:14:15 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,9 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  process.env.SECRET
   {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
   }

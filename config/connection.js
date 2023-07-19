bd27381e1ee910892f510d15bad8030eb3c2c342
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-18 20:30:55 -0500

    Merge branch 'tests' of github.com:konawave/event-lister into tests

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,8 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.SECRET
   {
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306,
   }

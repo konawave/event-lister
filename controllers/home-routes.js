60ededa4ef0b58a4ba495338d8ea5ff987e01dfa
konawave <nickkjeffs@gmail.com>
2023-07-23 21:21:15 -0400

    fuuujck

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -67,7 +67,7 @@ router.post('/login', async (req, res) => {
 
     if (!calUserData) {
       res
-        .status(400)
+        .status(401)
         .json({ message: 'Incorrect Username or password. Please try again!' });
       return;
     }
@@ -76,7 +76,7 @@ router.post('/login', async (req, res) => {
 
     if (!validPassword) {
       res
-        .status(400)
+        .status(402)
         .json({ message: 'Incorrect User name or password. Please try again!' });
       return;
     }
